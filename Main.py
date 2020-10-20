c0f090df7bb92e26f4be7dd1e710d94002945801
Dougc1 <72005771+Dougc1@users.noreply.github.com>
2020-10-20 11:20:37 +1100

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,9 +4,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas import DataFrame
-
-model = create_model("SPY.csv")
-predictions, etf_prices = test_predictions(model,"SPY-5-years.csv")
+data = "SPY-5-years.csv"
+model = create_model(data)
+predictions, etf_prices = test_predictions(model,"SPY.csv")
 p_list = predictions.tolist()
 balance = 1000000
 stock_count = 0
@@ -20,11 +20,11 @@ for i in range(0,len(p_list)):
         balance += stock_count*etf_price_list[i]
         stock_count = 0
     elif (prediction < 0.01 and prediction > 0.005):
-        stock_count += 15
-        balance -= etf_price_list[i]*15
+        stock_count += 25
+        balance -= etf_price_list[i]*25
     elif (prediction > 0.01):
-        stock_count += 30
-        balance -= etf_price_list[i]*30
+        stock_count += 50
+        balance -= etf_price_list[i]*50
     pl.append(((balance+etf_price_list[i]*stock_count)-1000000))
     days.append(i)
 
